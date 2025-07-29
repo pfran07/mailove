5424976a1682424558d0cc0046c0590208fd2d01
pfran07 <pierojorquera024@gmail.com>
2025-07-29 19:33:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const yesBtn = document.getElementById('yesBtn');
 const noBtn = document.getElementById('noBtn');
 
+yesBtn.style.transition = "transform 0.2s ease";
+
 yesBtn.addEventListener('click', () => {
   window.location.href = "yes_page.html";
 });
 
 let noTexts = [
-  "Seguro mai love 🥺",
+  "Seguro mai love?",
   "Piénsalo bien 😢",
   "No seas malo puto 😭",
   "Te doy sushi 🍣",
   "Y flores 🌹",
   "Solo di que sí 😞",
   "Por favor... 😞😞😞",
-  "Muajaja ahora estás obligado 😈"
+  "Muajaja, ahora estás obligado 😈"
 ];
 
 let count = 0;
@@ -21,5 +23,7 @@ let count = 0;
 noBtn.addEventListener('click', () => {
   noBtn.innerText = noTexts[count % noTexts.length];
   count++;
-  yesBtn.style.fontSize = `${20 + count * 5}px`;
+  yesBtn.style.transform = `scale(${1 + count * 0.2})`;
 });
+
+  
